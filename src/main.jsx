abd744aac5da6267d21d116db7c8966686bfcc03
Khushi Garg <khushigarg5412@gmail.com>
2026-06-03 21:48:02 +0530

    fix: typo

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'  
 import App from './App.jsx'
 import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
-import Layout from './layout.jsx';
+import Layout from './Layout.jsx';
 import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
