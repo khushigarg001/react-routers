db50acde8d0e6c5502eaaf3f28c4d04f17020750
Khushi Garg <khushigarg5412@gmail.com>
2026-06-03 21:53:15 +0530

    fix: typo

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Header from './components/header/Header'
+import Header from './components/Header/Header'
 import Home from './components/Home/Home'
-import Footer from './components/footer/Footer'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
